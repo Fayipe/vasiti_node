4e87323efe9bf8b0d02eccd453bb716056c10a6a
taiwofayipe <taiwofayipe@gmail.com>
2021-03-11 14:24:30 +0100

    new updates

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,14 +1,9 @@
 import errorHandler from "./errorHandler";
 import global from "./global";
 import { validation } from "./validation";
-import { LibraryFileUpload, PageFileUpload, ScriptFileUpload, destroyS3File } from "./uploads";
 
 export {
     global,
     validation,
-    errorHandler,
-    LibraryFileUpload,
-    PageFileUpload,
-    ScriptFileUpload,
-    destroyS3File
+    errorHandler
 };
